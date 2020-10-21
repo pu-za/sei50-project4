66f8c25ef2ded0c5834782b02e5a4101ccb3e2de
puja <gc.puja1@yahoo.com>
2020-10-21 12:41:17 +0100

    get singular emoji

--- emojis/urls.py
+++ emojis/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import EmojiList
+from .views import EmojiList, EmojiDetail
 
 urlpatterns = [
     path('', EmojiList.as_view()),
-    
+    path('<int:pk>/', EmojiDetail.as_view())
 ]
--- emojis/views.py
+++ emojis/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView 
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound
 
 from .models import Emoji
 from .serializers.common import EmojiSerializer
@@ -18,3 +19,17 @@ class EmojiList(APIView):
       newEmoji.save()
       return Response(newEmoji.data, status=status.HTTP_200_OK)
     return Response(newEmoji.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class EmojiDetail(APIView):
+  def get_one(self, pk):
+    try:
+      return Emoji.objects.get(pk=pk)
+    except Emoji.DoesNotExist:
+      return NotFound()
+  
+  def get(self, request, pk):
+    emoji = self.get_one(pk)
+    serializer = EmojiSerializer(emoji)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+  
